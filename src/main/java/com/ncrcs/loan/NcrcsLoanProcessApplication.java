819b020c419d2ee855e325fd680aa6bc8a2073bf
udeshikarathnayaka <udeshika.rathnayaka@surge.global>
2026-02-19 10:48:37 +0530

    Initial commit

---
+++ src/main/java/com/ncrcs/loan/NcrcsLoanProcessApplication.java
@@ -0,0 +1,13 @@
+package com.ncrcs.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcrcsLoanProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcrcsLoanProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncrcs/loan/NcrcsLoanProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncrcs.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcrcsLoanProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
